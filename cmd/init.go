c7ed860c3a2b346c4c5ab97e0ce956b96879f894
Roy van de Water <roy.vwater@gmail.com>
2019-07-30 16:18:02 -0700

    findOrCreate study file

--- cmd/init.go
+++ cmd/init.go
@@ -2,6 +2,8 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -9,11 +11,29 @@ import (
 var initCommand = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a config file for running a study",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("init called")
-	},
+	Run:   runInit,
+	Args:  cobra.MaximumNArgs(1),
 }
 
 func init() {
 	rootCmd.AddCommand(initCommand)
+}
+
+func runInit(cmd *cobra.Command, args []string) {
+	filename := getFilename(args)
+	file, err := os.Create(filename)
+	if os.IsExist(err) {
+		log.Fatalf("File at %v already exists, refusing to overwrite", filename)
+	}
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	fmt.Println("file", file)
+}
+
+func getFilename(args []string) string {
+	if len(args) > 0 {
+		return args[0]
+	}
+	return "study.json"
 }
